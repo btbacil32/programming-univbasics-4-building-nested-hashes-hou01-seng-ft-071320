9b7fbe43093b5f1712c3f94ffd018f9fffbf97f3
bacil thalji <btbacil32@gmail.com>
2020-07-05 08:12:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,11 @@ def monopoly_with_third_tier
   # complexity.
    {:railroads => {
      :pieces => 4,
-     :rent_in_dollars => {},
+     :rent_in_dollars => {
+       :one_piece_owned => 25,
+       :two_piece_owned => 50,
+       :
+     },
      :names => {}
    } 
  }
